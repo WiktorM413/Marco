f44bc5a6d6f7797458d120c570076b4d856eb692
Wiktor <wiktor.markowski81@gmail.com>
2026-08-30 16:06:15 +0200

    Declare the TomlReader methods

--- include/marco/toml/TomlReader.h
+++ include/marco/toml/TomlReader.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "marco/toml/Toml.h"
+#include "marco/toml/TomlError.h"
+#include <string>
+
+
 namespace Marco
 {
 	class TomlReader
@@ -7,6 +12,23 @@ namespace Marco
 	public:
 		TomlReader();
 
+		Toml Parse(const std::string& tomlString);
+		Toml Parse(std::istream&      tomlFile);
+
+		TomlError Error();
+		bool      IsValid();
+
+	private:
+		static TomlError FormTomlFromString(TomlValue& tomlValue, const std::string& tomlString, size_t& index);
+		static TomlError FormTomlValue     (TomlValue& tomlValue, const std::string& tomlString, size_t& index);
+		static TomlError HandleNumber      (TomlValue& tomlValue, const std::string& tomlString, size_t& index);
+		static TomlError HandleString      (TomlValue& tomlValue, const std::string& tomlString, size_t& index);
+		static TomlError HandleBool        (TomlValue& tomlValue, const std::string& tomlString, size_t& index);
+		static TomlError HandleNull        (TomlValue& tomlValue, const std::string& tomlString, size_t& index);
+		static TomlError HandleArray       (TomlValue& tomlValue, const std::string& tomlString, size_t& index);
+		static TomlError HandleEscape      (TomlValue& tomlValue, const std::string& tomlString, size_t& index);
 		
+		TomlError m_error;
+		bool      m_bool;
 	};
 }
