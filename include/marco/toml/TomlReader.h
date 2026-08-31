1dccd5d8d8feab9452c9288735d783d77e793184
Wiktor <wiktor.markowski81@gmail.com>
2026-08-31 08:06:01 +0200

    Modify comments for better clarity of where index is left at

--- include/marco/toml/TomlReader.h
+++ include/marco/toml/TomlReader.h
@@ -22,17 +22,17 @@ namespace Marco
 	private:
 		static TomlError FormTomlFromString(TomlValue& tomlValue, const std::string& tomlString, size_t& index);
 		static TomlError FormTomlValue     (TomlValue& tomlValue, const std::string& tomlString, size_t& index);
-		static TomlError HandleComment     (TomlValue& tomlValue, const std::string& tomlString, size_t& index);
+		static TomlError HandleComment     (TomlValue& tomlValue, const std::string& tomlString, size_t& index); // leaves index right after the first \n character it encounters
 		static TomlError HandleNumber      (TomlValue& tomlValue, const std::string& tomlString, size_t& index);
 		static TomlError HandleString      (TomlValue& tomlValue, const std::string& tomlString, size_t& index);
 		static TomlError HandleBool        (TomlValue& tomlValue, const std::string& tomlString, size_t& index);
 		static TomlError HandleNull        (TomlValue& tomlValue, const std::string& tomlString, size_t& index);
-		static TomlError HandleObject      (TomlValue& tomlValue, const std::string& tomlString, size_t& index);
+		static TomlError HandleObject      (TomlValue& tomlValue, const std::string& tomlString, size_t& index); // leaves index at the closest character that either isnt a '=' or a whitespace
 		static TomlError HandleArray       (TomlValue& tomlValue, const std::string& tomlString, size_t& index);
 		
-		static std::expected<std::string, TomlError> HandleStringKey       (const std::string& tomlString, size_t& index); // leaves index at the closest character after '='
-		static std::expected<std::string, TomlError> HandleStringLiteralKey(const std::string& tomlString, size_t& index); // leaves index at the closest character after '='
-		static std::expected<std::string, TomlError> HandleBareKey         (const std::string& tomlString, size_t& index); // leaves index at the closest character after '='
+		static std::expected<std::string, TomlError> HandleStringKey       (const std::string& tomlString, size_t& index); // leaves index at the closest character that either isnt a '=' or a whitespace
+		static std::expected<std::string, TomlError> HandleStringLiteralKey(const std::string& tomlString, size_t& index); // leaves index at the closest character that either isnt a '=' or a whitespace
+		static std::expected<std::string, TomlError> HandleBareKey         (const std::string& tomlString, size_t& index); // leaves index at the closest character that either isnt a '=' or a whitespace
 
 		static TomlError HandleEscape(std::string& value, const std::string& tomlString, size_t& index);
 		
