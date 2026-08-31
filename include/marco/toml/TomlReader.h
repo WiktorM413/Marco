2bbadb92587aabb7fcc233d13af9fc016b4c7565
Wiktor <wiktor.markowski81@gmail.com>
2026-08-31 18:30:25 +0200

    Add comment to TomlReader::HandleEscape to know where index is left at and att bool isMultiline to TomlReader::HandleString and TomlReader::HandleStringLiteral

--- include/marco/toml/TomlReader.h
+++ include/marco/toml/TomlReader.h
@@ -26,8 +26,8 @@ namespace Marco
 	
 		static TomlError FormTomlValue      (TomlValue& currTomlValue, const std::string& tomlString, size_t& index); // leaves index right after the first \n character it encounters
 		static TomlError HandleNumber       (TomlValue& currTomlValue, const std::string& tomlString, size_t& index);
-		static TomlError HandleString       (TomlValue& currTomlValue, const std::string& tomlString, size_t& index);
-		static TomlError HandleStringLiteral(TomlValue& currTomlValue, const std::string& tomlString, size_t& index);
+		static TomlError HandleString       (TomlValue& currTomlValue, const std::string& tomlString, size_t& index, bool isMultiline);
+		static TomlError HandleStringLiteral(TomlValue& currTomlValue, const std::string& tomlString, size_t& index, bool isMultiline);
 		static TomlError HandleBool         (TomlValue& currTomlValue, const std::string& tomlString, size_t& index);
 		static TomlError HandleNull         (TomlValue& currTomlValue, const std::string& tomlString, size_t& index);
 		static TomlError HandleArray        (TomlValue& currTomlValue, const std::string& tomlString, size_t& index);
@@ -39,7 +39,7 @@ namespace Marco
 		static std::expected<std::string, TomlError> HandleBareKey         (const std::string& tomlString, size_t& index); // leaves index at the closest character that either isnt a '=' or a whitespace
 
 		static TomlError HandleComment(const std::string& tomlString, size_t& index); // leaves index right after the first \n character it encounters
-		static TomlError HandleEscape(std::string& value, const std::string& tomlString, size_t& index);
+		static TomlError HandleEscape(std::string& value, const std::string& tomlString, size_t& index); // leaves index right after the 'X' as in \X
 
 		static std::expected<int, TomlError> ParseTomlInt(std::string_view s);
 		
